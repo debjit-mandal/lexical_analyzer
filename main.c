5d9a3cc39d73bc00c3701451677aa8d8c43b5f88
Debjit Mandal <115482933+debjit-mandal@users.noreply.github.com>
2023-05-31 13:07:44 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,8 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <string.h>
-#include <stdlib.h> // Include the <stdlib.h> header for malloc and free functions
+#include <stdlib.h> 
 
-// Token types
 typedef enum {
     IDENTIFIER,
     NUMBER,
@@ -13,13 +12,11 @@ typedef enum {
     KEYWORD
 } TokenType;
 
-// Token structure
 typedef struct {
     TokenType type;
     char value[50];
 } Token;
 
-// Function to check if a character is a valid operator
 bool isOperator(char c) {
     char operators[] = "+-*/=";
     for (int i = 0; i < strlen(operators); i++) {
@@ -29,7 +26,6 @@ bool isOperator(char c) {
     return false;
 }
 
-// Function to check if a character is a valid punctuation
 bool isPunctuation(char c) {
     char punctuations[] = ".,;(){}[]";
     for (int i = 0; i < strlen(punctuations); i++) {
@@ -39,7 +35,6 @@ bool isPunctuation(char c) {
     return false;
 }
 
-// Function to check if a string is a keyword
 bool isKeyword(char *str) {
     char *keywords[] = {"if", "else", "while", "for", "int", "float", "void", "return"};
     int numKeywords = sizeof(keywords) / sizeof(keywords[0]);
@@ -50,20 +45,17 @@ bool isKeyword(char *str) {
     return false;
 }
 
-// Function to tokenize the input string
 Token* tokenize(char *input) {
     Token *tokens = malloc(strlen(input) * sizeof(Token));
     int tokenCount = 0;
     int i = 0;
 
     while (input[i] != '\0') {
-        // Skip whitespaces
         if (isspace(input[i])) {
             i++;
             continue;
         }
 
-        // Identify identifiers or keywords
         if (isalpha(input[i])) {
             int j = 0;
             while (isalnum(input[i]) || input[i] == '_') {
@@ -81,7 +73,6 @@ Token* tokenize(char *input) {
             continue;
         }
 
-        // Identify numbers
         if (isdigit(input[i])) {
             int j = 0;
             while (isdigit(input[i]) || input[i] == '.') {
@@ -93,7 +84,6 @@ Token* tokenize(char *input) {
             continue;
         }
 
-        // Identify operators
         if (isOperator(input[i])) {
             tokens[tokenCount].value[0] = input[i++];
             tokens[tokenCount].value[1] = '\0';
@@ -102,7 +92,6 @@ Token* tokenize(char *input) {
             continue;
         }
 
-        // Identify punctuation
         if (isPunctuation(input[i])) {
             tokens[tokenCount].value[0] = input[i++];
             tokens[tokenCount].value[1] = '\0';
@@ -111,15 +100,13 @@ Token* tokenize(char *input) {
             continue;
         }
 
-        // Ignore unrecognized characters
         i++;
     }
 
-    tokens[tokenCount].type = -1;  // Mark end of tokens
+    tokens[tokenCount].type = -1;  
     return tokens;
 }
 
-// Function to print the tokens
 void printTokens(Token *tokens) {
     int i = 0;
     while (tokens[i].type != -1) {
@@ -133,7 +120,6 @@ int main() {
     printf("Enter an expression: ");
     fgets(input, sizeof(input), stdin);
 
-    // Remove the trailing newline character from fgets
     if (input[strlen(input) - 1] == '\n')
         input[strlen(input) - 1] = '\0';
 
